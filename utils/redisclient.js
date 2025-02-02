3f884648c22af392f2f2298d39134c1673d079d1
Piyush789-git <pikumar.jee@gmail.com>
2025-02-02 14:42:33 +0530

    Create redisclient.js

---
+++ utils/redisclient.js
@@ -0,0 +1,11 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+(async () => {
+  await client
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
+})();
+
+export { client };
